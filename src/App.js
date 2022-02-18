cb24fdd45449fdde192af788f999b45f3b97b4f8
Josue Garcia <yoshuah809@gmail.com>
2022-02-18 13:05:29 -0600

    repo initialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Algo Grande Viene por ahi</h1>
+		</div>
+	);
+}
+
+export default App;
